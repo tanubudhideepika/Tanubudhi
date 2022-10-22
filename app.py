15ee11b02a7e7df598dcd7c15fe232d5228ec959
Ubuntu <ubuntu@ip-172-31-1-169.us-west-1.compute.internal>
2022-10-22 04:39:36 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+        return render_template('index.html')
+@app.route("/about")
+def about_me():
+        return render_template('about_me.html') 
+
